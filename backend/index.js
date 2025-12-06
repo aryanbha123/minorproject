989db341751e14e174f856c79768b146d39958db
Aryan Bhandari <aryanbhandari@Aryans-MacBook-Air.local>
2025-12-06 09:00:12 +0530

    setup

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,21 @@
-import express from 'express'
-
+import express from 'express';
+import serverless from 'serverless-http';
 
 const app = express();
 
-
-app.get('/', (req,res)=>res.json({message:"Health Check working", success:true}));
-
-app.use((err, req, res, next) => {
-  console.error("ERROR:", err);
-
-  res.status(err.status || 500).json({
-    success: false,
-    message: err.message || "Internal server error",
-    stack: process.env.NODE_ENV === "development" ? err.stack : undefined
+app.get('/', (req, res) => {
+  res.json({
+    message: "Health Check working",
+    success: true
   });
 });
 
-app.listen(process.env.PORT , () => console.log("Server Live"));
+app.use((err, req, res, next) => {
+  console.error("ERROR:", err);
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal server error"
+  });
+});
+
+export default serverless(app);
